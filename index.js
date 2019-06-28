282997fb2f237d5207ebd08bf6b1d48b18213155
werronense <31091965+werronense@users.noreply.github.com>
2019-06-28 07:01:50 -0400

    Finish palindrome method

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
+module.exports = Phrase;
+
 // adds reverse to all strings
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
-}
-
-// reverses a string
-function reverse(string) {
-  return Array.from(string).reverse().join("");
 }
 
 // defines a phrase object
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function processor(string) {
-    return string.toLowerCase();
-  }
-
   // returns content processed for palindrome testing
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.letters().toLowerCase();
+  }
+
+  // returns the letters in the content
+  this.letters = function letters() {
+    return (this.content.match(/[a-zA-Z]/gi) || []).join("");
   }
 
   // returns true if palindrome, false otherwise
   this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
     return this.processedContent() === this.processedContent().reverse();
   }
 }
